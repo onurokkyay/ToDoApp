8b62e3e045598bcac563e3dbe0bf74f4d5031070
onurokkyay <onurokyay1@gmail.com>
2023-07-02 22:13:13 +0300

    Format Document with Prettier

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,107 +1,123 @@
-import './TodoApp.css'
-import {BrowserRouter, Routes, Route, useNavigate, useParams} from 'react-router-dom'
-import { useState } from 'react'
+import "./TodoApp.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
+import { useState } from "react";
 
-export default function TodoApp(){
-    return (
-        <div className="TodoApp">
-            <BrowserRouter>
-                <Routes>
-                    <Route path= '/' element= {<LoginComponent/>}> 
-                    </Route>
-                    <Route path= '/login' element= {<LoginComponent/>}> 
-                    </Route>
-                    <Route path= '/welcome/:userName' element= {<WelcomeComponent/>}> 
-                    </Route>
-                    <Route path= '*' element= {<ErrorComponent/>}> 
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        </div>
-    )
+export default function TodoApp() {
+  return (
+    <div className="TodoApp">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginComponent />}></Route>
+          <Route path="/login" element={<LoginComponent />}></Route>
+          <Route
+            path="/welcome/:userName"
+            element={<WelcomeComponent />}
+          ></Route>
+          <Route path="*" element={<ErrorComponent />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-function LoginComponent(){
+function LoginComponent() {
+  const [userName, setUserName] = useState("Onur Okyay");
 
-    const [userName, setUserName]= useState('Onur Okyay')
+  const [password, setPassword] = useState("");
 
-    const [password, setPassword]= useState('')
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
-    const [showSuccessMessage, setShowSuccessMessage]= useState(false)
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
 
-    const [showErrorMessage, setShowErrorMessage]= useState(false)
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  function handleUserNameChange(event) {
+    setUserName(event.target.value);
+  }
 
-    function handleUserNameChange(event){
-        setUserName(event.target.value)
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleSubmit() {
+    if (userName === "Onur Okyay" && password === "dummy") {
+      console.log("Success");
+      setShowSuccessMessage(true);
+      setShowErrorMessage(false);
+      navigate(`/welcome/${userName}`);
+    } else {
+      console.log("Failed");
+      setShowSuccessMessage(false);
+      setShowErrorMessage(true);
     }
+  }
 
-    function handlePasswordChange(event){
-        setPassword(event.target.value)
-    }
-
-    function handleSubmit(){
-        if(userName==='Onur Okyay' && password==='dummy'){
-            console.log('Success')
-            setShowSuccessMessage(true)
-            setShowErrorMessage(false)
-            navigate(`/welcome/${userName}`)
-        }
-        else {
-            console.log('Failed')
-            setShowSuccessMessage(false)
-            setShowErrorMessage(true)
-        }
-    }
-
-    return (
-        <div className="Login">
-            <h1>Time to login!</h1>
-            {showSuccessMessage && <div className='successMessage'>Authenticated Successfully</div>}
-            {showErrorMessage && <div className='errorMessage'>Authentication Failed. Please check your credentials.</div>}
-            <div className="LoginForm">
-                <div>
-                    <label>User Name</label>
-                    <input type="text" name="username" value= {userName}
-                    onChange = {handleUserNameChange}/>
-                </div>
-                <div>
-                    <label>Password</label>
-                    <input type="password" name="password" value= {password}
-                    onChange = {handlePasswordChange}/>
-                </div>
-                <div>
-                    <button type="button" name ="login" onClick={handleSubmit}>Login</button>
-                </div>
-            </div>
+  return (
+    <div className="Login">
+      <h1>Time to login!</h1>
+      {showSuccessMessage && (
+        <div className="successMessage">Authenticated Successfully</div>
+      )}
+      {showErrorMessage && (
+        <div className="errorMessage">
+          Authentication Failed. Please check your credentials.
         </div>
-    )
+      )}
+      <div className="LoginForm">
+        <div>
+          <label>User Name</label>
+          <input
+            type="text"
+            name="username"
+            value={userName}
+            onChange={handleUserNameChange}
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <div>
+          <button type="button" name="login" onClick={handleSubmit}>
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-function WelcomeComponent(){
+function WelcomeComponent() {
+  const params = useParams();
 
-    const params = useParams()
-
-    return (
-        <div className="Welcome">
-            <h1>Welcome {params.userName}</h1>
-            <div>
-                 Welcome Component
-            </div>
-
-        </div>
-    )
+  return (
+    <div className="Welcome">
+      <h1>Welcome {params.userName}</h1>
+      <div>Welcome Component</div>
+    </div>
+  );
 }
 
-function ErrorComponent(){
-    return (
-        <div className="ErrorComponent">
-            <h1>We are working really hard!</h1>
-            <div>
-                Apologies for the 404. Reach out team at ABC-DEF-GHIJ.
-            </div>
-        </div>
-    )
+function ErrorComponent() {
+  return (
+    <div className="ErrorComponent">
+      <h1>We are working really hard!</h1>
+      <div>Apologies for the 404. Reach out team at ABC-DEF-GHIJ.</div>
+    </div>
+  );
 }
 
+function ListTodosComponent() {
+  return <div className="ListTodosComponent"></div>;
+}
