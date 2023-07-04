d390d9f68aa750d7dbe8e93c361f01556435cd04
onurokkyay <onurokyay1@gmail.com>
2023-07-04 13:57:55 +0300

    Call Spring Boot ToDoService REST API, Invoking Spring Boot Hello World Bean & Path Param REST API from React, Refactoring Spring Boot REST API Invocation Code to New Module

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
 import { useParams, Link } from "react-router-dom";
+import { retrieveHelloWorldBean } from "./api/TodoService";
 function WelcomeComponent() {
-  const params = useParams();
+  const { userName } = useParams();
+
+  const [message, setMessage] = useState(null);
+
+  function callHelloWorld() {
+    console.log("called");
+    retrieveHelloWorldBean()
+      .then((response) => successfulResponse(response))
+      .catch((error) => errorResponse(error))
+      .finally(() => console.log("cleanup"));
+  }
+
+  function successfulResponse(response) {
+    setMessage(response.data.message);
+    console.log(response);
+  }
+
+  function errorResponse(error) {
+    console.log(error);
+  }
 
   return (
     <div className="Welcome">
-      <h1>Welcome {params.userName}</h1>
+      <h1>Welcome {userName}</h1>
       <div>
         Manage Your Todos. <Link to="/todos">Go Here </Link>
       </div>
+      <button className="btn btn-success m-5" onClick={callHelloWorld}>
+        Call Hello World
+      </button>
+      <div className="text-info">{message}</div>
     </div>
   );
 }
