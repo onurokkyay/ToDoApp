4b4119a070bab527ae0350384b5b42fe0ae4cf10
onurokkyay <onurokyay1@gmail.com>
2023-07-08 19:45:30 +0300

    Hello World Deleted

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -4,36 +4,12 @@ import { retrieveAllTodosForUserNameApi } from "./api/TodoService";
 function WelcomeComponent() {
   const { userName } = useParams();
 
-  const [message, setMessage] = useState(null);
-
-  function callHelloWorld() {
-    console.log("called" + userName);
-
-    retrieveAllTodosForUserNameApi(userName)
-      .then((response) => successfulResponse(response))
-      .catch((error) => errorResponse(error))
-      .finally(() => console.log("cleanup"));
-  }
-
-  function successfulResponse(response) {
-    //setMessage(response.data.message);
-    console.log(response);
-  }
-
-  function errorResponse(error) {
-    console.log(error);
-  }
-
   return (
     <div className="Welcome">
       <h1>Welcome {userName}</h1>
       <div>
         Manage Your Todos. <Link to="/todos">Go Here </Link>
       </div>
-      <button className="btn btn-success m-5" onClick={callHelloWorld}>
-        Call Hello World
-      </button>
-      <div className="text-info">{message}</div>
     </div>
   );
 }
